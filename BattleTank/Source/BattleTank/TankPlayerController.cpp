cd23f4098bad72938b125e6f122687e99b56d17d
ChrisCurreri <ccurreri95@gmail.com>
2017-12-01 15:29:27 -0500

    S4L112: Using Virtual and Override

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -3,7 +3,21 @@
 #include "TankPlayerController.h"
 
 
+void ATankPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"))
 
+	auto ControlledTank = GetControlledTank();
+	if (ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player posessing: %s"), *(ControlledTank->GetName()))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("Player is not possessing a controlled tank"))
+	}
+}
 
 ATank* ATankPlayerController::GetControlledTank() const 
 {
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -18,6 +18,6 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 public:
 	ATank* GetControlledTank() const;
 
-	
+	virtual void BeginPlay() override;
 	
 };
